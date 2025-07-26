d930455466a2c044b7f0cf98d82bd474b3345d8f
Hubert Kuch <kuchhubert@gmail.com>
2025-07-26 20:21:41 +0200

    feat: app

---
+++ src/test/java/pl/hubertkuch/GhreposApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hubertkuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhreposApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
